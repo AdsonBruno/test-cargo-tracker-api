27f7d0e23a5befe2692ecfbb40e91eb405073427
AdsonBruno <adsonbruno2@gmail.com>
2024-10-24 18:19:04 -0300

    feat: add method to validate cnpj

--- src/Services/CustomerService.cs
+++ src/Services/CustomerService.cs
@@ -19,7 +19,7 @@ namespace test_cargo_tracker_api.src.Services
         public async Task<ServiceResponse<CustomerModel>> CreateCustomer(CustomerModel newCustomer)
         {
             ServiceResponse<CustomerModel> serviceResponse = new ServiceResponse<CustomerModel>();
-
+            CnpjValidator cnpjValidator = new CnpjValidator(newCustomer.cnpj);
             try
             {
                 if (newCustomer == null)
@@ -31,15 +31,44 @@ namespace test_cargo_tracker_api.src.Services
                     return serviceResponse;
                 }
                 
-                if (!CpfValidator.IsValid(newCustomer.cpf))
+                if (!string.IsNullOrEmpty(newCustomer.cpf) && !string.IsNullOrEmpty(newCustomer.cnpj))
                 {
-                    serviceResponse.Success = false;
-                    serviceResponse.Message = "CPF invalid!";
-                    serviceResponse.statusCode = 400; 
+                    serviceResponse.Success = true;
+                    serviceResponse.Message = "Only one of CPF or CNPJ should be provided";
+                    serviceResponse.statusCode = 400;
                     return serviceResponse;
                 }
 
-                
+                if (!string.IsNullOrEmpty(newCustomer.cpf))
+                {
+                    if (!CpfValidator.IsValid(newCustomer.cpf))
+                    {
+                        serviceResponse.Success = false;
+                        serviceResponse.Message = "CPF invalid!";
+                        serviceResponse.statusCode = 400;
+                        return serviceResponse;
+                    }
+
+                }
+                else if (!string.IsNullOrEmpty(newCustomer.cnpj))
+                {
+                    if (!cnpjValidator.IsValid)
+                    {
+                        serviceResponse.Success = false;
+                        serviceResponse.Message = "CNPJ invalid";
+                        serviceResponse.statusCode = 400;
+                        return serviceResponse;
+                    }
+                }
+                else
+                {
+                    serviceResponse.Success = false;
+                    serviceResponse.Message = "Either CPF or CNPJ must be provided";
+                }
+
+
+
+
                 await _context.AddAsync(newCustomer);
                 await _context.SaveChangesAsync();
 
