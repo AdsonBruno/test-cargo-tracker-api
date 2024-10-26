62edd457315d5c343ee13e7110a02fc0f0105cc4
AdsonBruno <adsonbruno2@gmail.com>
2024-10-26 16:16:57 -0300

    feat: add container number validator

---
+++ src/Utils/Container/ContainerValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace test_cargo_tracker_api.src.Utils.Container
+{
+    public class ContainerValidator
+    {
+        public bool IsValidContainerNumber(string containerNumber)
+        {
+            string pattern = @"^[A-Z]{4}\d{7}$";
+
+            return Regex.IsMatch(containerNumber, pattern);
+        }
+    }
+}
