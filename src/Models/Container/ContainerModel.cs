6f87ca2a0fa9c0fb5e0c105d0241ab2c252821af
AdsonBruno <adsonbruno2@gmail.com>
2024-10-26 16:31:54 -0300

    refactor: refactor names to start in capital letters and for the Date to be in UTC

--- src/Models/Container/ContainerModel.cs
+++ src/Models/Container/ContainerModel.cs
@@ -9,10 +9,10 @@ namespace test_cargo_tracker_api.src.Models.Container
         public int Id { get; set; }
         public string ClientName { get; set; }
         public string ContainerNumber { get; set; }
-        public ContainerTypeEnum typeContainer {  get; set; }
-        public ContainerStatusEnum containerStatus { get; set; }
-        public ContainerCategoryEnum containerCategory { get; set; }
-        public DateTime CreationDate { get; set; } = DateTime.Now.ToLocalTime();
-        public DateTime DateOfLastUpdate { get; set; } = DateTime.Now.ToLocalTime();
+        public ContainerTypeEnum TypeContainer {  get; set; }
+        public ContainerStatusEnum ContainerStatus { get; set; }
+        public ContainerCategoryEnum ContainerCategory { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+        public DateTime DateOfLastUpdate { get; set; } = DateTime.UtcNow;
     }
 }
