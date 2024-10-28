9e0c64001a82bf5bfb69d33899e55c8802ba6047
AdsonBruno <adsonbruno2@gmail.com>
2024-10-28 09:25:50 -0300

    refactor: update migrations

---
+++ Migrations/20241027012054_updtate-model-container.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace test_cargo_tracker_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class updtatemodelcontainer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "typeContainer",
+                table: "Container",
+                newName: "TypeContainer");
+
+            migrationBuilder.RenameColumn(
+                name: "containerStatus",
+                table: "Container",
+                newName: "ContainerStatus");
+
+            migrationBuilder.RenameColumn(
+                name: "containerCategory",
+                table: "Container",
+                newName: "ContainerCategory");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TypeContainer",
+                table: "Container",
+                newName: "typeContainer");
+
+            migrationBuilder.RenameColumn(
+                name: "ContainerStatus",
+                table: "Container",
+                newName: "containerStatus");
+
+            migrationBuilder.RenameColumn(
+                name: "ContainerCategory",
+                table: "Container",
+                newName: "containerCategory");
+        }
+    }
+}
