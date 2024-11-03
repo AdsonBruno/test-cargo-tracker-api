0161a6f8b7d7d74a96dd1f22a75fe68a7effbbf5
AdsonBruno <adsonbruno2@gmail.com>
2024-11-03 11:19:42 -0300

    refactor: add strategy attribute

--- src/Services/Movements/IContainerMovement.cs
+++ src/Services/Movements/IContainerMovement.cs
@@ -1,10 +1,11 @@
 ﻿using test_cargo_tracker_api.src.Models;
 using test_cargo_tracker_api.src.Models.Movements;
+using test_cargo_tracker_api.src.Strategies.Movements;
 
 namespace test_cargo_tracker_api.src.Services.Movements
 {
     public interface IContainerMovement
     {
-        Task<ServiceResponse<MovementModel>> MoveContainer(MovementModel movement);
+        Task<ServiceResponse<MovementModel>> MoveContainer(MovementModel movement, IContainerMovementStrategy strategy);
     }
 }
